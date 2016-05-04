dda164c24d0f4d91dab5df68e503b11ffb1eb63a
Thac <thacdtd@gmail.com>
2016-05-04 14:07:12 +1000

    change redis config to http://144.6.229.1/

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module ChessRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
+    config.cache_store = :redis_store, 'redis://144.6.229.1:6379/0/cache', { expires_in: 90.minutes }
   end
 end
